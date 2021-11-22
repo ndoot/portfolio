36312deaee8d33eb5192b1b02f6077b0f99d0480
unknown <realneeldutta@gmail.com>
2021-11-22 18:38:26 +1100

    Working on selector

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 import Info from './components/Info';
-import Selector from './components/Selector';
+// import Selector from './components/Selector';
 import penguin from './penguin.jpg'
 function App() {
+  const [screen, setScreen] = React.useState('About');
   return (
     <div className = "layout">
       <Info
@@ -16,9 +17,11 @@ function App() {
         linkedin = "https://www.linkedin.com/in/neel-dutta-profile/"
       ></Info>
       <div className = "main">
-        <div className = "container">
-          <Selector></Selector>
-          <div className = "stopgap"></div>More Content
+        <div className = "container"> wtf
+          <div>POG</div>
+          { //<Selector screen={screen}></Selector>
+          }
+          More Content
         </div>
       </div>
     </div>
--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../App.css';
-const Selector = ({}) => {
+const Selector = ({screen}) => {
   return (
     <div className = "selector">
       <h1> About </h1>
@@ -10,7 +10,7 @@ const Selector = ({}) => {
 };
 
 Selector.propTypes = {
-  
+  screen: PropTypes.string,
 }
 
 export default Selector;
