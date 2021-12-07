c5975001fb7ac33a36f1c00b17adbfb92a8f6095
unknown <realneeldutta@gmail.com>
2021-12-07 23:59:23 +1100

    Got vanta working and header almost complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Info from './components/Info';
 
 function App() {
   return (
-    <main></main>
+    <main>
+      <Info></Info>
+    </main>
   );
 }
 
